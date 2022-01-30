c47c12b4593ba6028c0a6c60f4790609cdf801ce
Zeipher <96157647+Zeipher21x@users.noreply.github.com>
2022-01-30 04:11:19 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Requirements: Python
+Requirements: Python & Nmap
 
 
 # BlueBuff
@@ -8,3 +8,8 @@ Updates Linux And Scans For Open Ports
 # BlueBuff 0.2
 Cleaned up Spaghetti Code
 (Next Version Will Display Service Version For Open Ports) 
+
+# BlueBuff 0.3
+Added Log Backups To local directory 
+Added Parsing Logs For Keywords "sudo|CRON|su" And Ability To Save Results To Text File 
+Added Localhost Nmap service Scan And Ability To Save Results To Text File 
--- bluebuff.py
+++ bluebuff.py
@@ -1,12 +1,14 @@
 #IMPORTS
+from asyncio import subprocess
 import os 
 import socket 
-#SETTING HOST AND PORT SETTINGS FOR BASIC PORT SCAN
-hosts = ["Localhost"]
-ports = [22, 23, 80, 443, 445, 3389]
+import subprocess
+import getpass
+
 #SETTING OS VARIABLE 
 operating = os.name
 
+#ASCII ART FTW
 motd = ("""
             ▄▄                                            ▄▄▄▄   ▄▄▄▄
 ▀███▀▀▀██▄▀███                    ▀███▀▀▀██▄            ▄█▀ ▀▀ ▄█▀ ▀▀
@@ -32,10 +34,14 @@ motd = ("""
            ▒▒▓                                 ▒▒                                 
          ▒▒▒                                 ▒ ▒ ▒▒                               
          
-version 0.2                                                                                            
+version 0.3                                                                                           
 """)
-
+#OS CHECK FOR PROPER OS 
 def detect_os():
+    print ("-"*120)
+    print ("Detecting For Linux To Run Script")
+    print ("-"*120)
+
     if operating == "posix":
        os.system("clear")
        print(motd)
@@ -45,33 +51,75 @@ def detect_os():
        os.system('date')   
        os.system("uptime")
        os.system('hostname')
-
+    else:
+        print ("This Script Is Only For Linux...")   
+#LINUX UPGRADE & UPDATE 
 def update_upgrade():
+    print ("-"*120)
+    print("Starting Update/Upgrade Module")
+    print ("-"*120)
+
     update = input("Would You Like To Update? (yes/no) (REQUIRES SUDO)")
     if update == "yes":
         os.system("sudo apt-get update && sudo apt-get upgrade")
     elif update == "no":
-        input("Please Press Enter To Continue with scanning: ")
+        print ("-"*120)
+        print ("Skipping Upgrade Sequence")        
+        print ("-"*120)
+#BACKUP LOG DIRECTORY
+def backup_logs():
+    print ("-"*120)
+    print("Starting Backup_logs Module")
+    print ("-"*120)
 
-def scanner():
-    for host in hosts:
-     for port in ports:
-        try:
-            print ("[+] Connecting to " + host + ":" + str(port))
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(5)
-            result = s.connect_ex((host, port))
-            if result == 0:
-                print ("  [*] Port " + str(port) + " open!")
-            s.close()
-        except:
-            pass
 
+    name = input("Please enter your username for this account: ")
+    print("Backing up logs to /home/" +name+ "/log_backups")
+    os.system("mkdir log_backups")
+    os.system("sudo rsync -a /var/log/   /home/" +name+ "/log_backups/")
+    print("Backup Complete To bluebuff folder")
+
+    view_logs_in_textfile =input("Would you like to search logs for suspicious keywords and save to a text file? ")
+    if view_logs_in_textfile == "yes" or "Yes" or "y":
+        with open('log_keywords.txt', 'w') as f:
+            out = subprocess.run('sudo find  -name  "*.log"  | sudo cat /home/'+name+'/log_backups/auth.log | grep -E -i "sudo|cron|su|ssh"', shell=True, stdout=f, text=True)
+            print ("-"*120)
+            print ("log_keywords.txt saved to bluebuff folder")
+            print ("-"*120)
+    elif view_logs_in_textfile =="no" or "No" or "n":
+        print ("-"*120)
+        print ("Skipping Backup Sequence")        
+        print ("-"*120)
+#LOCALHOST SERVICE SCAN
+def Nmap_scan():
+    print ("-"*120)
+    print("Starting Scanner Module")
+    print ("-"*120)
+
+    ask = input("Would You Like To Save Results To A Text File? *FYI: Saying No Will Print Scan Results To Console*")
+
+    if ask == "yes" or "Yes" or "y":
+        print ("-"*120)
+        print("Starting Nmap Service Scan, Please Be Patience...")
+        print ("-"*120)
+        with open('Nmap_output.txt', 'a') as f:
+            out = subprocess.run('nmap -sV localhost', shell=True, stdout=f, text=True)
+    elif ask ==  "no" or "No" or "n":
+        print ("-"*120)
+        print("Starting Nmap Service Scan, Please Be Patience...")
+        print ("-"*120)
+        os.system("sudo nmap -sV localhost")
+    print ("-"*120)
+    print("Saved Nmap Scan To Local Directory")
+    print ("-"*120)
+
+#MAIN FOR CALLING ALL FUNCTIONS
 def main():
     detect_os()
     update_upgrade()
-    scanner()
-
+    backup_logs()
+    Nmap_scan()
+#MAIN RUN 
 if __name__ == '__main__':
     main()
 
